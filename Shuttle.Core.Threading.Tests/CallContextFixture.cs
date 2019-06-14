2f64b3ab40f817a5248a75984830d8fc979922c5
Eben <me@ebenroux.co.za>
2019-06-14 13:33:01 +0200

    - call context

---
+++ Shuttle.Core.Threading.Tests/CallContextFixture.cs
@@ -0,0 +1,67 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Shuttle.Core.Threading.Tests
+{
+    [TestFixture]
+    public class CallContextFixture
+    {
+        [Test]
+        public void Should_be_able_to_flow_data()
+        {
+            var d1 = new object();
+            var t1 = default(object);
+            var t10 = default(object);
+            var t11 = default(object);
+            var t12 = default(object);
+            var t13 = default(object);
+            var d2 = new object();
+            var t2 = default(object);
+            var t20 = default(object);
+            var t21 = default(object);
+            var t22 = default(object);
+            var t23 = default(object);
+
+            Task.WaitAll(
+                Task.Run(() =>
+                {
+                    CallContext.SetData("d1", d1);
+                    new Thread(() => t10 = CallContext.GetData("d1")).Start();
+                    Task.WaitAll(
+                        Task.Run(() => t1 = CallContext.GetData("d1"))
+                            .ContinueWith(t => Task.Run(() => t11 = CallContext.GetData("d1"))),
+                        Task.Run(() => t12 = CallContext.GetData("d1")),
+                        Task.Run(() => t13 = CallContext.GetData("d1"))
+                    );
+                }),
+                Task.Run(() =>
+                {
+                    CallContext.SetData("d2", d2);
+                    new Thread(() => t20 = CallContext.GetData("d2")).Start();
+                    Task.WaitAll(
+                        Task.Run(() => t2 = CallContext.GetData("d2"))
+                            .ContinueWith(t => Task.Run(() => t21 = CallContext.GetData("d2"))),
+                        Task.Run(() => t22 = CallContext.GetData("d2")),
+                        Task.Run(() => t23 = CallContext.GetData("d2"))
+                    );
+                })
+            );
+
+            Assert.That(d1, Is.SameAs(t1));
+            Assert.That(d1, Is.SameAs(t10));
+            Assert.That(d1, Is.SameAs(t11));
+            Assert.That(d1, Is.SameAs(t12));
+            Assert.That(d1, Is.SameAs(t13));
+
+            Assert.That(d2, Is.SameAs(t2));
+            Assert.That(d2, Is.SameAs(t20));
+            Assert.That(d2, Is.SameAs(t21));
+            Assert.That(d2, Is.SameAs(t22));
+            Assert.That(d2, Is.SameAs(t23));
+
+            Assert.Null(CallContext.GetData("d1"));
+            Assert.Null(CallContext.GetData("d2"));
+        }
+    }
+}
---
+++ Shuttle.Core.Threading/CallContext.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Concurrent;
+using System.Threading;
+using Shuttle.Core.Contract;
+
+namespace Shuttle.Core.Threading
+{
+    // shout-out to Daniel Cazzulino: http://www.cazzulino.com/callcontext-netstandard-netcore.html
+    public static class CallContext
+    {
+        private static readonly ConcurrentDictionary<string, AsyncLocal<object>> State = new ConcurrentDictionary<string, AsyncLocal<object>>();
+
+        public static void SetData(string name, object data)
+        {
+            Guard.AgainstNullOrEmptyString(name, nameof(name));
+
+            State.GetOrAdd(name, _ => new AsyncLocal<object>()).Value = data;
+        }
+
+        public static object GetData(string name)
+        {
+            Guard.AgainstNullOrEmptyString(name, nameof(name));
+
+            return State.TryGetValue(name, out var data) ? data.Value : null;
+        }
+
+        public static object RemoveData(string name)
+        {
+            Guard.AgainstNullOrEmptyString(name, nameof(name));
+
+            return State.TryRemove(name, out var data) ? data.Value : null;
+        }
+    }
+}
