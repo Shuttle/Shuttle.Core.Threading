571381cd97e4a35852f4f344342149fadd3ec097
Eben <me@ebenroux.co.za>
2022-06-27 16:14:31 +0200

    - package upgrade

--- Shuttle.Core.Threading/Properties/AssemblyInfo.cs
+++ Shuttle.Core.Threading/Properties/AssemblyInfo.cs
@@ -13,10 +13,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle(".NET Standard")]
 #endif
 
-[assembly: AssemblyVersion("11.1.1.0")]
-[assembly: AssemblyCopyright("Copyright (c) 2021, Eben Roux")]
+[assembly: AssemblyVersion("11.1.2.0")]
+[assembly: AssemblyCopyright("Copyright (c) 2022, Eben Roux")]
 [assembly: AssemblyProduct("Shuttle.Core.Threading")]
 [assembly: AssemblyCompany("Eben Roux")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("11.1.1")]
+[assembly: AssemblyInformationalVersion("11.1.2")]
 [assembly: ComVisible(false)]
